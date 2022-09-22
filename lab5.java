67572d3248b9b1bfdc10149af2ac9fdd8ef202ec
Nik Bailey <bail0301@morris.umn.edu>
2022-09-22 14:12:03 -0500

    hashmap going both way
    Hashmaps that go from char companies to int programmers and also from int programmers to char companies

--- lab5.java
+++ lab5.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 import java.util.ArrayList;
@@ -11,12 +10,19 @@ public class lab5 {
         System.out.println("Enter number of programmers and companies: ");
         int amount = Integer.valueOf(input.nextLine());
 
-        HashMap<Integer, ArrayList<Character>> preferenceArray = makePMap(amount);
-        System.out.println(preferenceArray);
+        HashMap<Integer, ArrayList<Character>> pPrefArray = makeProgMap(amount);
+        HashMap<Character, ArrayList<Integer>> cPrefArray = makeComMap(amount);
+        System.out.println(pPrefArray);
+        System.out.println(cPrefArray);
+
+        input.close();
 
     }
 
-    public static ArrayList<Character> preferenceArray(int n) {
+/* 
+    
+*/
+    public static ArrayList<Character> ProgPrefArray(int n) { // Boolean: true, company pref, false, programmer pref
         ArrayList<Character> pArray = new ArrayList<Character>(n);
 
         for (int i = 0; i < n; i++) { 
@@ -26,10 +32,28 @@ public class lab5 {
         return pArray;
     }
 
-    public static HashMap<Integer, ArrayList<Character>> makePMap(int n) {
+    public static HashMap<Integer, ArrayList<Character>> makeProgMap(int n) {
         HashMap<Integer, ArrayList<Character>> pmap = new HashMap<Integer, ArrayList<Character>>();
         for (Integer i = 1; i <= n; i++) {
-            pmap.put(i, preferenceArray(n));
+            pmap.put(i, ProgPrefArray(n));
+        }
+        return pmap;
+    }
+
+    public static ArrayList<Integer> ComPrefArray(int n) { // Boolean: true, company pref, false, programmer pref
+        ArrayList<Integer> pArray = new ArrayList<Integer>(n);
+
+        for (int i = 0; i < n; i++) { 
+            pArray.add(i, i+1);
+        }
+        Collections.shuffle(pArray);
+        return pArray;
+    }
+
+    public static HashMap<Character, ArrayList<Integer>> makeComMap(int n) {
+        HashMap<Character, ArrayList<Integer>> pmap = new HashMap<Character, ArrayList<Integer>>();
+        for (Integer i = 0; i < n; i++) {
+            pmap.put((char)(i+'a'), ComPrefArray(n));
         }
         return pmap;
     }
