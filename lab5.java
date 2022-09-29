f11141ac102bc8746a1f43c5159fac1bb3c37fef
Nik Bailey <bail0301@morris.umn.edu>
2022-09-29 14:34:24 -0500

    Added likeability code
    Added code to calculate the likeability of companies and programmers

--- lab5.java
+++ lab5.java
@@ -14,12 +14,15 @@ public class lab5 {
         HashMap<Character, ArrayList<Integer>> cPrefArray = makeComMap(amount);
         System.out.println(pPrefArray);
         System.out.println(cPrefArray);
+        System.out.println(GetLikeabilityScores(pPrefArray, cPrefArray));
 
         input.close();
 
     }
 
     /* Creates the preferences for a single programmer
+     * Uses input n to create an array from 1-n
+     * Then shuffles the array to randomize the order
      */
     public static ArrayList<Character> ProgPrefArray(int n) { // Boolean: true, company pref, false, programmer pref
         ArrayList<Character> pArray = new ArrayList<Character>(n);
@@ -32,6 +35,10 @@ public class lab5 {
     }
 
     /* Creates lists of each programmers preferences
+     * Creates a hashmap of the programmers preferences
+     * Spot 1 is mapped to programmer one's preferences,
+     * Spot 2 is mapped to programmer two's preferences,
+     * etc.
      */
     public static HashMap<Integer, ArrayList<Character>> makeProgMap(int n) {
         HashMap<Integer, ArrayList<Character>> pmap = new HashMap<Integer, ArrayList<Character>>();
@@ -42,6 +49,8 @@ public class lab5 {
     }
 
     /* Creates the preference for a single company
+     * Uses input n to create an array from 1-n
+     * Then shuffles the array to randomize the order
      */
     public static ArrayList<Integer> ComPrefArray(int n) { // Boolean: true, company pref, false, programmer pref
         ArrayList<Integer> pArray = new ArrayList<Integer>(n);
@@ -54,6 +63,10 @@ public class lab5 {
     }
 
     /* Creates lists of each companies preferences
+     * Creates a hashmap of the companies preferences
+     * Spot 1 is mapped to programmer one's preferences
+     * Spot 2 is mapped to programmer two's preferences
+     * etc.
      */
     public static HashMap<Character, ArrayList<Integer>> makeComMap(int n) {
         HashMap<Character, ArrayList<Integer>> pmap = new HashMap<Character, ArrayList<Integer>>();
@@ -61,5 +74,86 @@ public class lab5 {
             pmap.put((char)(i+'a'), ComPrefArray(n));
         }
         return pmap;
+    }
+
+    /* Determines how optimal each pairing of
+     * a company and a programmer is
+     */
+    public static int[] GetLikeabilityScores(HashMap<Integer, ArrayList<Character>> programPref, HashMap<Character, ArrayList<Integer>> compPref) {
+        int[] likeabilityScores = new int[programPref.entrySet().size()]; // Creates an array of the same size as the programPref entry set
+        // for (int i=1; i <= programPref.entrySet().size();) {
+        //     likeabilityScores[i] = ComputeLikeability(i, programPref, compPref); // Computing how optimal a specific pairing is
+        // }
+        return likeabilityScores;
+    }
+
+    /* Computes companies likeability
+     * with different programmers
+     */
+    public static int[] ComputeCompanyLikeability(int programmer, HashMap<Integer, ArrayList<Character>> programPref, HashMap<Character, ArrayList<Integer>> compPref) {
+        // list of programmers company preferences
+        ArrayList<Character> companies = programPref.get(programmer);
+
+        // list containing the preferences of each company, where index 0 is company a, 1 b etc.
+        ArrayList<ArrayList<Integer>> programmers = new ArrayList<ArrayList<Integer>>();
+        for (int i=0; i < companies.size(); i++) {
+            programmers.add(i, compPref.get((Character)(char)(i+'a')));
+        }
+
+        // array of numerical value for how fit a programmer is to each company
+        int[] likeabilities = new int[companies.size()];
+
+        // max fitness to a company (first choice)
+        int maxLike = companies.size();
+        
+        // find the programmers fitness to each company, assign value to likeabilities array
+        // start at company a (0), see which position company is in programmers preferred company array
+        for (int com = 0; com < companies.size(); com++) {
+            int dislike = 0;
+
+            // for each position in the array a company is away from the programmers top preference, increment dislike
+            for (int i=0; (Character)(char)(i+'a') == companies.get(i); i++) {
+                dislike++;
+            }
+            
+            likeabilities[com] = maxLike-dislike;
+        }
+        
+        return likeabilities;
+    }
+
+    /* Computes programmers likeability
+     * with different companies
+     */
+    public static int[] ComputeProgrammerLikeability(int company, HashMap<Integer, ArrayList<Character>> programPref, HashMap<Character, ArrayList<Integer>> compPref) {
+        // list of companies programmer preferences
+        ArrayList<Integer> programmers = compPref.get(company);
+        
+        // list containing the preferences of each programmer, where index 0 is company a, 1 b etc.
+        ArrayList<ArrayList<Character>> companies = new ArrayList<ArrayList<Character>>();
+        for (int i=0; i < programmers.size(); i++) {
+            companies.add(i, programPref.get((Integer)(int)(i - 'a')));
+        }
+
+        // array of numerical value for how fit a programmer is to each company
+        int[] likeabilities = new int[programmers.size()];
+        
+        // max fitness to a company (first choice)
+        int maxLike = programmers.size();
+
+        // find the programmers fitness to each company, assign value to likeabilities array
+        // start at company a (0), see which position company is in programmers preferred company array
+        for(int prog = 0; prog < programmers.size(); prog++) {
+            int dislike = 0;
+
+            // for each position in the array a company is away from the programmers top preference, increment dislike
+            for (int i = 0; (Integer)(int)(i - 'a') == programmers.get(i); i++) {
+                dislike++;
+            }
+
+            likeabilities[prog] = maxLike - dislike;
+        }
+
+        return likeabilities;
     }
 }
